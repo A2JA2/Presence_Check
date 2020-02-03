699fe03aebb703d5a32b6ba330cd6ca457ed6bab
1321200 <55232721+A2JA2@users.noreply.github.com>
2020-02-03 12:11:52 +0000

    Add files via upload

---
+++ Presence Check/Presence Check/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Presence_Check
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtInput.text))
+            {
+                MessageBox.Show("The input box is emty - input is required.");
+                return;
+            }
+            else
+            {
+                txtInput.text = "";
+                MessageBox.Show("Input accepted");
+                return;
+            }
+        }
+    }
+}
